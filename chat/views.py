ee5359affd44707db2bfef7bc7231fb1194cfbbf
mohammad <mr.dori.dev@gmail.com>
2022-03-25 12:46:35 +0430

    add room view

--- chat/views.py
+++ chat/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def index(request: object):
     return render(request, "chat/index.html")
+
+
+def room(request: object, room_name: str):
+    context = {
+        "room_name": room_name
+    }
+    return render(request, "chat/room.html", context)
