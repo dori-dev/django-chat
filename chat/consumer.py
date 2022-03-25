99f380168b549451baf1780a976449a105402adc
mohammad <mr.dori.dev@gmail.com>
2022-03-25 13:30:56 +0430

    add consumer

---
+++ chat/consumer.py
@@ -0,0 +1,20 @@
+"""chat consumer
+"""
+from typing import Union
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data: Union[str, bytes]):
+        text_data_json: dict = json.loads(text_data)
+        message: str = text_data_json['message']
+        self.send(text_data=json.dumps({
+            'message': message*2
+        }))
