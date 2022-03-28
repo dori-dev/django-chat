4588c39b22e1223a76e3e209f6ea24bc99477975
mohammad <mr.dori.dev@gmail.com>
2022-03-28 12:49:11 +0430

    add message model

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("author", "content", "timestamp")
+
+
+admin.site.register(Message, MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    @staticmethod
+    def last_messages():
+        return Message.objects.order_by("-timestamp").all()
+
+    def author_username(self):
+        return self.author.username
+
+    def __str__(self):
+        return "message"  # TODO use chat_room and author, ....
