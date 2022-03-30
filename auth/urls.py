b36842d91de5729ae6f3420b53a7e0b3ae65a0d1
mohammad <mr.dori.dev@gmail.com>
2022-03-30 10:41:52 +0430

    complete auth app

--- auth/urls.py
+++ auth/urls.py
@@ -3,7 +3,7 @@
 from django.urls import path
 from . import views
 
-app_name = "accounts"
+app_name = "auth"
 urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
