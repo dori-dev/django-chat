4408d9ea51c1f71fec05962e328ca846d395a068
mohammad <mr.dori.dev@gmail.com>
2022-03-31 11:14:05 +0430

    add send image

--- chat/consumer.py
+++ chat/consumer.py
@@ -15,12 +15,13 @@ class ChatConsumer(WebsocketConsumer):
 
     def new_message(self, data: dict):
         author = User.objects.get(username=data['username'])
-        room = Chat.objects.get(room_id=data['room_name'])  # TODO
+        room = Chat.objects.get(room_id=data['room_name'])
         # use `objects.create` because I just want `insert` message!
         Message.objects.create(
             author=author,
             content=data['message'],
-            room=room)
+            room=room,
+            command=)
 
     def fetch_message(self, room_name: str):
         query_set: QuerySet[Message] = Message.last_messages(room_name)
@@ -33,6 +34,10 @@ class ChatConsumer(WebsocketConsumer):
                     "author": message['author_username']
                 }
             )
+
+    def send_image(self, data: dict):
+        # self.new_message(data)
+        self.send_to_room(data)
 
     def message_serializer(self, query_set: QuerySet[Message]):
         serialized: list = MessageSerializer(query_set, many=True)
@@ -60,35 +65,39 @@ class ChatConsumer(WebsocketConsumer):
         Args:
             text_data (str): text data
         """
-        text_data_dict: dict = json.loads(text_data)
-        command: str = text_data_dict['command']
+        data_dict: dict = json.loads(text_data)
+        command: str = data_dict['command']
         # execute the function according to the given `command`
         if command == 'new_message':
-            message: str = text_data_dict['message']
+            message: str = data_dict['message']
             if not message.strip():
                 return
-            self.new_message(text_data_dict)
-            self.send_to_room(text_data_dict)
+            self.new_message(data_dict)
+            self.send_to_room(data_dict)
         elif command == 'fetch_message':
-            room_name: str = text_data_dict['room_name']
+            room_name: str = data_dict['room_name']
             self.fetch_message(room_name)
+        elif command == 'send_image':
+            self.send_image(data_dict)
         else:
             print(f'Invalid Command: "{command}"')
 
-    def send_to_room(self, text_data_dict):
+    def send_to_room(self, data: dict):
         """send message to room group
 
         Args:
             message (str): message will send
         """
-        message: str = text_data_dict['message']
-        author: str = text_data_dict['username']
+        message: str = data['message']
+        author: str = data['username']
+        command: str = data.get("command", "new_message")
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
                 'author': author,
+                'command': command,
             }
         )
 
